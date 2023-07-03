d1288162b150a00ced94208c1457eec60ddb4ade
Yan-Hao-Wang <bses30074@gmail.com>
2023-07-03 22:07:53 +0800

    http.get get the content of left section

--- root/scripts/index.js
+++ root/scripts/index.js
@@ -5,41 +5,92 @@ editor.session.setMode("ace/mode/text"); // Set editor syntax to asciidoc
 
 var output_session = document.querySelector("#output"); // output session set to id="output" tag in html
 
-// Use http.get to get config.json
-var configFile=new XMLHttpRequest();
-configFile.open("GET", "./config.json", false);
-var configText_json ="";
-var configText="";
+let configFile = "";
+async function fetchConfig() {
+  try {
+    const response = await fetch('config.json');
+    const data = await response.json();
+    configFile = data;
+    // Continue with further processing or use of the configFile variable
+  } catch (error) {
+    console.error(error);
+  }
+}
+fetchConfig();
 
-var before_url="test_default"
-// // Change file button function
-// var button = document.querySelector('.change_file');
-// function popup3(e) {
-//    var guest = window.prompt('Change the left adoc file with freebsd document url', before_url);
-//    if (guest == null || "") {
-//       console.log('yes')
-//    } 
-//    else {
-//       console.log('no')
-//    }
-// }
-// button.addEventListener('click', popup3);
+// Get the man_page_map.json as variable first
+let json_map = ""
+fetch('other/man_page_map.json')
+  .then(response => response.json())
+  .then(data => {
+    json_map = data
+  })
+  .catch(error => console.error(error));
 
+function searchKey(jsonObj, key) {
+  let result = null;
+  for (const prop in jsonObj) {
+    if (prop === key) {
+      return jsonObj[prop];
+    } else if (typeof jsonObj[prop] === 'object') {
+      result = searchKey(jsonObj[prop], key);
+      if (result !== null) {
+        return result;
+      }
+    }
+  }
+  return result;
+}
+
+function github_get(url){
+  // Github api url to get .adoc file
+  github_url = url;
+  var request = new XMLHttpRequest();
+  request.open("GET", github_url, false);
+  request.send(null);
+  if (request.status === 200) {
+    // Success - handle the response
+    response_text = request.responseText
+    editor.setValue("") // Clean content
+    editor.session.insert(editor.getCursorPosition(), request.responseText); // Insert .adoc content that github api get to left editor session
+    window.origin_content = response_text; // Use global window to store content     
+  } 
+  else {
+     // Error - handle the error condition
+     alert("Error occurred. Status: " + request.status + "\nYou enter wrong url.");
+  }
+}
 
 function search_content() {
-   var input1 = document.getElementById("input1").value;
-   var selectOption = document.getElementById("selectMenu").value;
-   // Do something with the input values and selected option
-   console.log("Input 1:", input1);
-   console.log("Selected Option:", selectOption);
-   if(selectOption == "option0") {
+  var input1 = document.getElementById("input1").value;
+  var selectOption = document.getElementById("selectMenu").value;
+  var search_key = input1 + '.' +  selectOption.charAt(selectOption.length - 1);
+  var github_raw_url = configFile["src_github_raw"]
+  
+  if (selectOption == "option0") {
+    for (i = 1; i < 10; i++) {
+      search_key = input1 + '.' +  i.toString();
+      const result = searchKey(json_map, search_key);
+      if (result !== null) {
+        github_raw_url = github_raw_url + result.substr(9, result.length);
+        console.log(github_raw_url)
+        github_get(github_raw_url)   
+        return;
+      }
+    }
+  }
+  else if (selectOption == "optionn") { // option n
 
-   }
-   else if(selectOption == "option10") {
-
-   }
-   else {
-
-   }
-   // Output warning message
- }
+  }
+  else {
+    const result = searchKey(json_map, search_key);
+    if (result !== null) {
+      github_raw_url = github_raw_url + result.substr(9, result.length);
+      console.log(github_raw_url)
+      github_get(github_raw_url)   
+      return;
+    }
+  }
+  window.alert("Search no result")
+  // Output warning message
+}
