085ee885c3e5edfc050e28aa78f39c0750d54628
Wang-Yan-Hao <bses30074@gmail.com>
2023-07-03 13:29:56 +0800

    first commit

---
+++ root/scripts/index.js
@@ -0,0 +1,23 @@
+// Ace editor setting
+var editor = ace.edit("editor"); // Set editor to id="editor" tag in html
+editor.setOption("wrap", "free"); // Long lines will automatically wrap to the next line when they reach the edge of the editor, without inserting line breaks or truncating the content.
+editor.session.setMode("ace/mode/text"); // Set editor syntax to asciidoc
+
+var output_session = document.querySelector("#output"); // output session set to id="output" tag in html
+
+// Use http.get to get config.json
+var configFile=new XMLHttpRequest();
+configFile.open("GET", "./config.json", false);
+var configText_json ="";
+var configText="";
+
+// Change file button function
+var button = document.querySelector('.change_file');
+function popup3(e) {
+   var guest = window.prompt('Change the left adoc file with freebsd document url', before_url);
+   if (guest == null || "") {
+   } 
+   else {
+   }
+}
+button.addEventListener('click', popup3);
