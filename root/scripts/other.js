7464646eb5388f2f0b6b7e625fb263ca7026457f
Yan-Hao-Wang <bses30074@gmail.com>
2023-08-02 14:11:32 +0800

    intergrate the css and add dragline from document editor

--- root/scripts/other.js
+++ root/scripts/other.js
@@ -7,6 +7,46 @@ window.addEventListener('beforeunload', function(e) {
     return confirmationMessage;
 });
 
+// Middle line dragging
+let isDragging = false;
+let containerRect, dragLine, leftDiv, rightDiv;
+
+function startDrag(event) {
+    isDragging = true;
+    containerRect = document.querySelector('.editor-container').getBoundingClientRect();
+    dragLine = document.getElementById('dragLine');
+    leftDiv = document.getElementById('left');
+    rightDiv = document.getElementById('right');
+    // Disable pointer events on #output to allow dragging over it
+
+    var outputElement = document.getElementById('output');
+    outputElement.style.pointerEvents = 'none';      
+}
+
+document.addEventListener('mousemove', drag);
+document.addEventListener('mouseup', stopDrag);
+
+function drag(event) {
+    if (isDragging) {
+    const dragX = event.clientX - containerRect.left;
+
+    if (dragX >= 0 && dragX <= containerRect.width) {
+        leftDiv.style.width = (dragX / containerRect.width) * 100 + '%';
+        rightDiv.style.width = 100 - (dragX / containerRect.width) * 100 + '%';
+        dragLine.style.left = (dragX - dragLine.clientWidth / 2) + 'px';
+    }
+    }
+}
+
+function stopDrag() {
+    isDragging = false;
+    var editor = ace.edit("editor")
+    editor.resize();
+    // Re-enable pointer events on #output after dragging
+    var outputElement = document.getElementById('output');
+    outputElement.style.pointerEvents = '';     
+}
+
 // While edit content after 0.5 second right sesstion will rerender
 let debounceTimeoutId = null; // To prevent too many function calls
 // Create a new observer instance
