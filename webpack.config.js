c6666b4832f31cb5e16d464d3d67a9d58163832c
Yan-Hao-Wang <bses30074@gmail.com>
2024-01-10 05:49:18 +0800

    Set the config of "webpack"

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+
+module.exports = {
+	mode: 'production',
+	entry: {
+		bundle: ['./src/index.js'],
+	},
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'build'),
+	},
+	optimization: {
+		minimize: false,
+		minimizer: [new TerserPlugin()],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+			filename: 'index.html',
+			chunks: ['bundle'],
+			inject: 'body',
+		}),
+		new HtmlWebpackPlugin({
+			template: './public/pages/help.html',
+			filename: 'pages/help.html',
+			chunks: [],
+		}),
+		new CopyWebpackPlugin({
+			patterns: [
+				{ from: 'public/styles', to: 'styles' },
+				{ from: 'public/config.json', to: 'config.json' },
+				{ from: 'public/favicon.ico', to: 'favicon.ico' },
+				{ from: 'public/other', to: 'other' },
+			],
+		}),
+	],
+}
